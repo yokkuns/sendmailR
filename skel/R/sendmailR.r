65d40c52547dc15679696b911e054ea64ef77c53
yokkuns <yokkuns@tkul.jp>
2012-12-09 17:25:43 +0900

    change to be able to set the Content-Type

--- skel/R/sendmailR.r
+++ skel/R/sendmailR.r
@@ -16,6 +16,7 @@
 .write_mail <- function(headers, msg, sock) {
   if (!is.list(msg))
     msg <- list(msg)
+  content.type <- ifelse("Content-Type" %in% names(headers), headers$`Content-Type`, "text/plain; format=flowed")
 
   ## Generate MIME headers:
   boundary <- paste(packBits(sample(0:1, 256, TRUE)), collapse="")
@@ -35,7 +36,7 @@
       .write_mime_part(part, sock)
     else if (is.character(part)) { ## Legacy support for plain old string
       ## writeLines(sprintf("--%s", boundary), sock, sep="\r\n")
-      writeLines("Content-Type: text/plain; format=flowed\r\n", sock, sep="\r\n")
+      writeLines(paste("Content-Type: ",content.type,"\r\n",sep=""), sock, sep="\r\n")
       writeLines(part, sock, sep="\r\n")
     }
   }
