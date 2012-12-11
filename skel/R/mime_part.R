880a491c3cf4b6b3c2648568bef6d74fc63895a8
yokkuns <yokkuns@tkul.jp>
2012-12-11 22:33:23 +0900

    add Content-ID of attachment files

--- skel/R/mime_part.R
+++ skel/R/mime_part.R
@@ -30,7 +30,8 @@
     name <- basename(fn)
 
   text <- base64encode(fn, linewidth=72, newline="\n")
-  headers <- list("Content-Type"=type,
+  headers <- list("Content-ID"=sprintf("<%s>",name),
+                  "Content-Type"=type,
                   "Content-Disposition"=sprintf("%s; filename=%s",
                     disposition, name),
                   "Content-Transfer-Encoding"="base64")
